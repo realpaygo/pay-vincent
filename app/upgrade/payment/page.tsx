8a274019b1ebe8943d087574a4181d8623ffc116
derexcom001 <chipgraminfo@gmail.com>
2025-06-23 13:00:09 -0700

    Update page.tsx

--- app/upgrade/payment/page.tsx
+++ app/upgrade/payment/page.tsx
@@ -32,7 +32,7 @@ export default function UpgradePaymentPage() {
   }
 
   const handleCopyAccountNumber = () => {
-    navigator.clipboard.writeText("2079154877")
+    navigator.clipboard.writeText("2076219357")
     setCopiedAccount(true)
     setTimeout(() => setCopiedAccount(false), 2000)
   }
@@ -93,7 +93,7 @@ export default function UpgradePaymentPage() {
             <div>
               <p className="text-gray-700 mb-1">Account Number</p>
               <div className="flex items-center justify-between">
-                <p className="font-bold">2079154877</p>
+                <p className="font-bold">2076219357</p>
                 <button
                   onClick={handleCopyAccountNumber}
                   className="bg-orange-400 text-white px-3 py-1 rounded text-sm"
@@ -110,7 +110,7 @@ export default function UpgradePaymentPage() {
 
             <div>
               <p className="text-gray-700 mb-1">Account Name</p>
-              <p className="font-bold">ADAMU MANSUR</p>
+              <p className="font-bold">Nwite Sabastine Ebuka</p>
             </div>
           </div>
 
